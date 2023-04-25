9f76d8c4a0b52e684270d314a0127bcc3007f0dd
s1072954 <ryan0918204599@gmail.com>
2023-04-25 12:58:25 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from typing import Union
+from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+key = os.getenv("FAKE_VALUE")
+app = FastAPI()
+
+@app.get("/")
+async def main():
+    return {"message": key}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
